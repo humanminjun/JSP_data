ce3fbe038e46539fc76e91857b0887c46387b2f4
shs010608 <shs010608@naver.com>
2022-07-07 17:59:11 +0900

    게시판 관리 최종

--- chap11/src/main/java/com/saeyan/dao/BoardDAO.java
+++ chap11/src/main/java/com/saeyan/dao/BoardDAO.java
@@ -139,7 +139,7 @@ public class BoardDAO {
 	}
 	
 	public void updateBoard(BoardVO bVo) {
-		String sql = "update board set name=?, email=?, pass=?, title=?, content=?, where num=?";
+		String sql = "update board set name=?, email=?, pass=?, title=?, content=? where num=?";
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
